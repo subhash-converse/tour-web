87c35e41faf9d9c5b8b7d32b7db9f46e6820bfdb
sasisubhash <subhashkrishna1973@gmail.com>
2023-08-11 12:17:21 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <FontAwesomeIcon icon={faUser} />
+    </div>
+  );
+}
+
+export default App;
